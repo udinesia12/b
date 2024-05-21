06484be61348a04114bd4a948c06b451860de4fa
udinesia <udinesia1204@gmail.com>
2024-05-22 00:53:08 +0700

    bangun datar

---
+++ src/aplikasi_12/Aplikasi_12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_12;
+
+/**
+ *
+ * @author HP
+ */
+public class Aplikasi_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
